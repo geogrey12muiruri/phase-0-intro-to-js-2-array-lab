890527d23819ec51102ec302f5892e985e97848e
geogrey12muiruri <greggambugua@gmail.com>
2022-08-18 20:27:08 +0300

    completed the assaignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+function appendCat() {
+    let newCatOrder = [...cats, "Broom"]
+    return newCatOrder;
+   
+}
+function prependCat(name) {
+    let newCatOrder = ["Arnold", ...cats,]
+    return newCatOrder;
+}
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
